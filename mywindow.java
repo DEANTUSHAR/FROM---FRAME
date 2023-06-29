f18a56f45bfbaa65334ecbc945920dde889a4379
Tushar sharma <85436672+DEANTUSHAR@users.noreply.github.com>
2023-06-29 15:34:53 +0530

    THIS IS DEMO FORM  CREATED WITH JAVA MANNUALY

---
+++ mywindow.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+
+
+class mywindow extends JFrame
+{
+    
+    public mywindow()
+    {
+           super.setTitle("FORM");
+           super.setSize(400,400);
+           super.setLocation(100, 200);
+           super.setVisible(true);
+           
+		System.out.println("this is  sub class");
+		
+	}
+  
+}
